f0fb559b0a6ab1d3e959e81302fb2d757d045f82
korenk7 <55340703+korenk7@users.noreply.github.com>
2022-06-21 08:27:13 +0300

    Update Task6.java

--- eclipse-workspace/jd1-part3-Strings-all/src/by/javacourse/module2/stringlikestring/Task6.java
+++ eclipse-workspace/jd1-part3-Strings-all/src/by/javacourse/module2/stringlikestring/Task6.java
@@ -1,7 +1,7 @@
 package by.javacourse.module2.stringlikestring;
 
 public class Task6 {
-	// Из заданной строки получить новую, повторив каждый символ дважды
+	// РР· Р·Р°РґР°РЅРЅРѕР№ СЃС‚СЂРѕРєРё РїРѕР»СѓС‡РёС‚СЊ РЅРѕРІСѓСЋ, РїРѕРІС‚РѕСЂРёРІ РєР°Р¶РґС‹Р№ СЃРёРјРІРѕР» РґРІР°Р¶РґС‹
 	public static void main(String[] args) {
 
 		String s = "A pilot  in the 123 awakes one morning to see, standing before him, the most extraordinary little fellow. \"Please,\" asks the word, \"draw me a sheep.\" And the pil345345345ot realizes that when life's events are too difficult to understand, 345 is no word but to succumb to their mysteries. He pulls out pencil and paper... And 234 begins this wise and enchanting fable that, in teaching the secret of what is really 234 in life, has changed forever the world for its readworders.a";
