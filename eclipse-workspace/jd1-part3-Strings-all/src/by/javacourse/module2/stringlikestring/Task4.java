0abb135c2a9f87a44eec6793f4f3997438b9c4c3
korenk7 <55340703+korenk7@users.noreply.github.com>
2022-06-21 08:26:31 +0300

    Update Task4.java

--- eclipse-workspace/jd1-part3-Strings-all/src/by/javacourse/module2/stringlikestring/Task4.java
+++ eclipse-workspace/jd1-part3-Strings-all/src/by/javacourse/module2/stringlikestring/Task4.java
@@ -1,11 +1,11 @@
 package by.javacourse.module2.stringlikestring;
 
 public class Task4 {
-	// с помощью функций копирования и конкантетации составить из слова
-	// "информатика" слово "торт"
+	// СЃ РїРѕРјРѕС‰СЊСЋ С„СѓРЅРєС†РёР№ РєРѕРїРёСЂРѕРІР°РЅРёСЏ Рё РєРѕРЅРєР°РЅС‚РµС‚Р°С†РёРё СЃРѕСЃС‚Р°РІРёС‚СЊ РёР· СЃР»РѕРІР°
+	// "РёРЅС„РѕСЂРјР°С‚РёРєР°" СЃР»РѕРІРѕ "С‚РѕСЂС‚"
 	public static void main(String[] args) {
 
-		String s = "информатика";
+		String s = "ГЁГ­ГґГ®Г°Г¬Г ГІГЁГЄГ ";
 		StringBuilder newStr = new StringBuilder();
 		
 		newStr.append(s.charAt(7)).append(s.substring(3, 5)).append(s.charAt(7));
