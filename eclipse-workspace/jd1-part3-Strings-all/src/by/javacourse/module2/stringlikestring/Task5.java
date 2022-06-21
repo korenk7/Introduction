fdaf7774583ca79c001c5e82df7659db5f28de50
korenk7 <55340703+korenk7@users.noreply.github.com>
2022-06-21 08:26:52 +0300

    Update Task5.java

--- eclipse-workspace/jd1-part3-Strings-all/src/by/javacourse/module2/stringlikestring/Task5.java
+++ eclipse-workspace/jd1-part3-Strings-all/src/by/javacourse/module2/stringlikestring/Task5.java
@@ -1,7 +1,7 @@
 package by.javacourse.module2.stringlikestring;
 
 public class Task5 {
-	// Посчитать сколько раз среди символов заданной строки встречается буква "a"
+	// РџРѕСЃС‡РёС‚Р°С‚СЊ СЃРєРѕР»СЊРєРѕ СЂР°Р· СЃСЂРµРґРё СЃРёРјРІРѕР»РѕРІ Р·Р°РґР°РЅРЅРѕР№ СЃС‚СЂРѕРєРё РІСЃС‚СЂРµС‡Р°РµС‚СЃСЏ Р±СѓРєРІР° "a"
 	public static void main(String[] args) {
 
 		String s = "A pilot  in the 123 awakes one morning to see, standing before him, the most extraordinary little fellow. \"Please,\" asks the word, \"draw me a sheep.\" And the pil345345345ot realizes that when life's events are too difficult to understand, 345 is no word but to succumb to their mysteries. He pulls out pencil and paper... And 234 begins this wise and enchanting fable that, in teaching the secret of what is really 234 in life, has changed forever the world for its readworders.a";
